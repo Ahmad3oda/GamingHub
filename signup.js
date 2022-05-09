fb633f59a4d5b09864966931f658fedc2fafb5d1
3oda <104653414+3oda@users.noreply.github.com>
2022-05-09 09:51:52 +0200

    Update signup.js

--- signup.js
+++ signup.js
@@ -3,6 +3,9 @@ let mail = document.getElementById('mail');
 let pass = document.getElementById('pass');
 let pas = document.getElementById('pas');
 
+
+
+
 txt.onkeyup = function () {
     localStorage.setItem("txt", txt.value);
 }
@@ -17,6 +20,8 @@ pass.onkeyup = function () {
 pas.onkeyup = function () {
     localStorage.setItem("pas", pas.value);
 }
+
+
 
 
 
@@ -65,15 +70,47 @@ icon.onclick = function(){
 // localStorage.setItem("pass",pass);
 // localStorage.setItem("pas",pas);
 
-window.onload=function(){
-    text.focus();
+// window.onload=function(){
+//     text.focus();
+
+// }
+// text.oncontextmenu=function(){
+//     text.blur()
+// }
+
+// mySub.onclick=function(){
+//     mySub.style.background='white'
+// }
+
+//console.log(localStorage.pas);
+
+function fun()
+{
+let one= localStorage.pas;
+   let two= localStorage.pass;
+    let check = document.getElementById('alert');
+    if(one==two)
+    {
+        return true;
+    }
+    else
+    {
+        check.style.display = 'block';
+        return false;
+        
+    }
 
 }
-text.oncontextmenu=function(){
-    text.blur()
-}
 
-
-mySub.onclick=function(){
-    mySub.style.background='white'
-}
+//    let one= localStorage.pas;
+//    let two= localStorage.pass;
+  
+//     if(one==two)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
